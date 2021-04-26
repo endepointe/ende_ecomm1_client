b2ba0a16d1a8fcbc62a7654c0ca0bc0e69463fee
EndePointe <endepointe@gmail.com>
2021-04-26 22:59:45 +0100

    switched to nextjs

--- server/routes/test.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-  // res.json({ test: 'at the test, redirecting' });
-  res.redirect('http://google.com');
-});
-
-module.exports = router;
